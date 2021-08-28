f7b035331f8d22227f3a1166c7dca2d07b06c38e
rich61016 <rich61016@gmail.com>
2021-08-28 13:28:19 +0800

    Initial Commit

---
+++ FlickerDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlickerDemo
+//
+//  Created by YA on 2021/8/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
